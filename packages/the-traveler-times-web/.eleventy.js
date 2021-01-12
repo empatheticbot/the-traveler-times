1740fa1163698d9ccee30cf480b45a9d6bda457a
Steve Ledsworth <sledsworth@gmail.com>
2021-01-12 21:26:47 +0000

    initial commut

---
+++ packages/the-traveler-times-web/.eleventy.js
@@ -0,0 +1,70 @@
+const { DateTime } = require("luxon");
+const fs = require("fs");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addFilter("noWhiteSpace", (string) => {
+    console.log(string);
+    return string.replace(/\s/g, "");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "LLL dd, yyyy"
+    );
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  eleventyConfig.addFilter("keywordsFromTags", (list, filter = []) => {
+    return list.filter((item) => filter.indexOf(item) == -1).join(", ");
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  eleventyConfig.addPassthroughCopy("assets");
+
+  // Browsersync Overrides
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync("_site/404.html");
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+    ui: false,
+    ghostMode: false,
+  });
+
+  return {
+    templateFormats: ["md", "njk", "html", "liquid", "css"],
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    // These are all optional, defaults are shown:
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
+};
