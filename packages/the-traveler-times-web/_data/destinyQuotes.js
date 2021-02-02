3f190a1a8a6a54dd0d10e8c29b31e7201fdbd016
Steve Ledsworth <sledsworth@gmail.com>
2021-02-02 16:18:52 -0500

    more quotes

--- packages/the-traveler-times-web/_data/destinyQuotes.js
+++ packages/the-traveler-times-web/_data/destinyQuotes.js
@@ -1,1 +1,13 @@
-module.exports = {};
+module.exports = {
+  quotes: [
+    {
+      quote: "The quest for knowledge is the purest war.",
+      author: "Destiny 2; 7th Book of Sorrow, 11th Understanding, I.VI",
+    },
+    {
+      quote:
+        "Be ever violent as you rage against the ignorance that threatens to stall your growth.",
+      author: "Destiny 2; 7th Book of Sorrow, 11th Understanding, I.V",
+    },
+  ],
+};
