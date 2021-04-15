ebab7ea64c87945263fa95da17bd080b5e5aab94
Steve Ledsworth <sledsworth@gmail.com>
2021-04-15 00:19:23 -0400

    setup initial durable object. still not working yet

---
+++ packages/d2-definitions-crawler/src/D2DefinitionsDurableObject.mjs
@@ -0,0 +1,107 @@
+import { BungieAPIHandler } from '@the-traveler-times/bungie-api-gateway'
+import { chunkArray } from '@the-traveler-times/utils'
+
+const blacklist = ['DestinyInventoryItemDefinition']
+
+export class Destiny2DefinitionsDurableObject {
+  constructor(state, env) {
+    this.state = state
+    this.env = env
+  }
+
+  // async initialize() {
+  //   let stored = await this.state.storage.get('value')
+  //   this.value = stored || 0
+  // }
+
+  async fetch(request) {
+    const definitionWorkerUrl = await this.env.DESTINY_2_DEFINITIONS.get(
+      'DEFINITION_WORKER_URL'
+    )
+
+    const apiKey = await this.env.BUNGIE_API.get('KEY')
+    const oauthToken = await this.env.BUNGIE_API.get('OAUTH_TOKEN')
+    const membershipId = await this.env.BUNGIE_API.get('MEMBERSHIP_ID')
+    const membershipType = await this.env.BUNGIE_API.get('MEMBERSHIP_TYPE')
+    const characterId = await this.env.BUNGIE_API.get('CHARACTER_ID')
+
+    const bungieAPIHandler = new BungieAPIHandler({
+      apiKey,
+      oauthToken,
+      membershipId,
+      membershipType,
+      characterId,
+    })
+
+    const manifest = await bungieAPIHandler.getManifest()
+    const definitionsObject = { ...manifest.jsonWorldComponentContentPaths.en }
+
+    const definitions = Object.entries(definitionsObject).reduce(
+      (arr, pair) => {
+        const [definition, url] = pair
+        if (blacklist.includes(definition)) {
+          return arr
+        }
+        arr.push({ definition, url })
+        return arr
+      },
+      []
+    )
+
+    const chunkedDefinitions = chunkArray(definitions, 5)
+    const definitionRequests = []
+    for (const defs of chunkedDefinitions) {
+      definitionRequests.push(
+        fetch(definitionWorkerUrl, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json;charset=utf-8',
+          },
+          body: JSON.stringify({ definitions: defs }),
+        })
+      )
+    }
+
+    const allDefinitions = await Promise.all(definitionRequests)
+    return new Response(JSON.stringify({ definitions: allDefinitions }), {
+      status: 200,
+      'content-type': 'application/json;charset=utf-8',
+    })
+  }
+
+  // Handle HTTP requests from clients.
+  // async fetch(request) {
+  //   // Make sure we're fully initialized from storage.
+  //   if (!this.initializePromise) {
+  //     this.initializePromise = this.initialize().catch(err => {
+  //       // If anything throws during initialization then we need to be
+  //       // sure sure that a future request will retry initialize().
+  //       // Note that the concurrency involved in resetting this shared
+  //       // promise on an error can be tricky to get right -- we don't
+  //       // recommend customizing it.
+  //       this.initializePromise = undefined
+  //       throw err
+  //     })
+  //   }
+  //   await this.initializePromise
+
+  //   // Apply requested action.
+  //   let url = new URL(request.url)
+  //   let currentValue = this.value
+  //   switch (url.pathname) {
+  //     case '/increment':
+  //       currentValue = ++this.value
+  //       await this.state.storage.put('value', this.value)
+  //       break
+  //     case '/decrement':
+  //       currentValue = --this.value
+  //       await this.state.storage.put('value', this.value)
+  //       break
+  //     case '/':
+  //       // Just serve the current value. No storage calls needed!
+  //       break
+  //     default:
+  //       return new Response('Not found', { status: 404 })
+  //   }
+  // }
+}
--- packages/d2-definitions-crawler/src/index.mjs
+++ packages/d2-definitions-crawler/src/index.mjs
@@ -1,64 +1,82 @@
-import { BungieAPIHandler } from '@the-traveler-times/bungie-api-gateway'
-import { chunkArray } from '@the-traveler-times/utils'
+// import { BungieAPIHandler } from '@the-traveler-times/bungie-api-gateway'
+// import { chunkArray } from '@the-traveler-times/utils'
 
-const blacklist = ['DestinyInventoryItemDefinition']
+// const blacklist = ['DestinyInventoryItemDefinition']
+
+// export default {
+//   async fetch(request, env) {
+//     const definitionWorkerUrl = await env.DESTINY_2_DEFINITIONS.get(
+//       'DEFINITION_WORKER_URL'
+//     )
+
+//     const apiKey = await env.BUNGIE_API.get('KEY')
+//     const oauthToken = await env.BUNGIE_API.get('OAUTH_TOKEN')
+//     const membershipId = await env.BUNGIE_API.get('MEMBERSHIP_ID')
+//     const membershipType = await env.BUNGIE_API.get('MEMBERSHIP_TYPE')
+//     const characterId = await env.BUNGIE_API.get('CHARACTER_ID')
+
+//     const bungieAPIHandler = new BungieAPIHandler({
+//       apiKey,
+//       oauthToken,
+//       membershipId,
+//       membershipType,
+//       characterId,
+//     })
+
+//     const manifest = await bungieAPIHandler.getManifest()
+//     const definitionsObject = { ...manifest.jsonWorldComponentContentPaths.en }
+
+//     const definitions = Object.entries(definitionsObject).reduce(
+//       (arr, pair) => {
+//         const [definition, url] = pair
+//         if (blacklist.includes(definition)) {
+//           return arr
+//         }
+//         arr.push({ definition, url })
+//         return arr
+//       },
+//       []
+//     )
+
+//     const chunkedDefinitions = chunkArray(definitions, 5)
+//     const definitionRequests = []
+//     for (const defs of chunkedDefinitions) {
+//       definitionRequests.push(
+//         fetch(definitionWorkerUrl, {
+//           method: 'POST',
+//           headers: {
+//             'content-type': 'application/json;charset=utf-8',
+//           },
+//           body: JSON.stringify({ definitions: defs }),
+//         })
+//       )
+//     }
+
+//     const allDefinitions = await Promise.all(definitionRequests)
+
+//     return new Response(JSON.stringify({ definitions: allDefinitions }), {
+//       headers: {
+//         'content-type': 'application/json;charset=UTF-8',
+//         status: 200,
+//       },
+//     })
+//   },
+// }
+
+export { Destiny2DefinitionsDurableObject } from './D2DefinitionsDurableObject.mjs'
 
 export default {
   async fetch(request, env) {
-    const definitionWorkerUrl = await env.DESTINY_2_DEFINITIONS.get(
-      'DEFINITION_WORKER_URL'
-    )
-
-    const apiKey = await env.BUNGIE_API.get('KEY')
-    const oauthToken = await env.BUNGIE_API.get('OAUTH_TOKEN')
-    const membershipId = await env.BUNGIE_API.get('MEMBERSHIP_ID')
-    const membershipType = await env.BUNGIE_API.get('MEMBERSHIP_TYPE')
-    const characterId = await env.BUNGIE_API.get('CHARACTER_ID')
-
-    const bungieAPIHandler = new BungieAPIHandler({
-      apiKey,
-      oauthToken,
-      membershipId,
-      membershipType,
-      characterId,
-    })
-
-    const manifest = await bungieAPIHandler.getManifest()
-    const definitionsObject = { ...manifest.jsonWorldComponentContentPaths.en }
-
-    const definitions = Object.entries(definitionsObject).reduce(
-      (arr, pair) => {
-        const [definition, url] = pair
-        if (blacklist.includes(definition)) {
-          return arr
-        }
-        arr.push({ definition, url })
-        return arr
-      },
-      []
-    )
-
-    const chunkedDefinitions = chunkArray(definitions, 5)
-    const definitionRequests = []
-    for (const defs of chunkedDefinitions) {
-      definitionRequests.push(
-        fetch(definitionWorkerUrl, {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json;charset=utf-8',
-          },
-          body: JSON.stringify({ definitions: defs }),
-        })
+    try {
+      console.log(request, env, env.D2_DEFINITIONS_OBJECT)
+      const id = env.D2_DEFINITIONS_OBJECT.idFromName(
+        'definition-durable-object'
       )
+      const stub = env.D2_DEFINITIONS_OBJECT.get(id)
+      const response = await stub.fetch(request)
+      return response
+    } catch (e) {
+      return new Response(e.message)
     }
-
-    const allDefinitions = await Promise.all(definitionRequests)
-
-    return new Response(JSON.stringify({ definitions: allDefinitions }), {
-      headers: {
-        'content-type': 'application/json;charset=UTF-8',
-        status: 200,
-      },
-    })
   },
 }
