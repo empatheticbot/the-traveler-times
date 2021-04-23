24171ee1e32b3f73397b6287e9993d49bc6207ab
Steve Ledsworth <sledsworth@gmail.com>
2021-04-22 20:21:26 -0400

    started to fetch activities for nightfall

--- packages/d2-nightfall-worker/src/index.mjs
+++ packages/d2-nightfall-worker/src/index.mjs
@@ -1,18 +1,31 @@
-import { PublicMilestoneHandler } from '@the-traveler-times/bungie-api-gateway'
+import {
+  PublicMilestoneHandler,
+  ActivityHandler,
+} from '@the-traveler-times/bungie-api-gateway'
 
 export default {
   async fetch(request, env) {
     const publicMilestoneHandler = new PublicMilestoneHandler()
     await publicMilestoneHandler.init(env.BUNGIE_API)
+    const activityHandler = new ActivityHandler()
+    await activityHandler.init(env.BUNGIE_API)
 
     try {
-      const nightfallMilestone = await publicMilestoneHandler.getPublicMilestone(
+      const nightfallMilestone = await publicMilestoneHandler.getPublicMilestoneByHash(
         '1942283261',
       )
+      console.log(env.DESTINY_2_DEFINITIONS)
+      const activities = await activityHandler.getActivitiesByHash(
+        nightfallMilestone.activities.map(activity => activity.activityHash),
+        env.DESTINY_2_DEFINITIONS,
+      )
 
-      return new Response(JSON.stringify(nightfallMilestone), {
-        status: 200,
-      })
+      return new Response(
+        JSON.stringify({ ...nightfallMilestone, activities }),
+        {
+          status: 200,
+        },
+      )
     } catch (e) {
       return new Response(e, {
         status: 500,
