3b32bc84ca89bf0c2f8b8a99f806d99cf987ab62
Steve Ledsworth <sledsworth@gmail.com>
2021-04-28 21:04:57 -0400

    added xur isAvailable flag

--- packages/d2-xur-worker/src/index.mjs
+++ packages/d2-xur-worker/src/index.mjs
@@ -14,12 +14,25 @@ export default {
       await twitterHandler.init(env.TWITTER_API)
       const xur = await vendorHandler.getStrippedDownVendorByHash(Hashes.XUR)
 
+      const currentDate = new Date()
+      const xurLeaveDate = new Date(xur.lastRefreshDate)
+      xurLeaveDate.setDate(xurLeaveDate.getDate() + 4)
+
+      if (currentDate > xurLeaveDate) {
+        return new Response(JSON.stringify({ ...xur, isAvailable: false }), {
+          status: 200,
+        })
+      }
+
       const searchDate = new Date(xur.lastRefreshDate)
       const location = await twitterHandler.getXurLocation(searchDate)
 
-      return new Response(JSON.stringify({ ...xur, location }), {
-        status: 200,
-      })
+      return new Response(
+        JSON.stringify({ ...xur, location, isAvailable: true }),
+        {
+          status: 200,
+        },
+      )
     } catch (e) {
       return new Response(e.message, {
         status: 500,
