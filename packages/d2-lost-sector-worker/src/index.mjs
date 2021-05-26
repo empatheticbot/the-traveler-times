c6e2e077f79e0f4b10d8238cc921e47c20b6de5d
Steve Ledsworth <sledsworth@gmail.com>
2021-05-25 22:48:06 -0400

    added lost sector modifier ignore list for common modifiers

--- packages/d2-lost-sector-worker/src/index.mjs
+++ packages/d2-lost-sector-worker/src/index.mjs
@@ -1,10 +1,27 @@
 import { DefinitionHandler } from '@the-traveler-times/bungie-api-gateway'
 import { getCurrentLostSectorHashes } from './LostSectorHandler'
 
+const modifierIgnoreList = [
+  '2687456355', // Champions: Cabal
+  '1930311099', // Champions: Vex
+  '3495411183', // Champions: Taken
+  '2834348323', // Champions: Mob
+  '2055950944', // Champions: Fallen
+  '3605663348', // Champions: Hive
+  '2078602635', // Champions: All
+  '939324719', // Equipment Locked
+  '3859784314', // Match Game
+  '2821775453', // Master Modifiers
+  '2301442403', // Legend Modifiers
+  '376634891', // Limited Revives
+]
+
 async function getLostSectorData(lostSector, definitionHandler) {
   const activity = await definitionHandler.getActivity(lostSector.hash)
   const modifiers = await definitionHandler.getActivityModifiers(
-    ...activity.modifiers.map(modifier => modifier.activityModifierHash),
+    ...activity.modifiers
+      .map(modifier => modifier.activityModifierHash)
+      .filter(hash => !modifierIgnoreList.includes(hash)),
   )
 
   const destination = await definitionHandler.getDestination(
