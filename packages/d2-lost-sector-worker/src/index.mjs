e2a67f8f05551adfa968002294e417b56b329554
Steve Ledsworth <sledsworth@gmail.com>
2021-06-02 23:01:43 -0400

    added some additional details around modifieres of interest for lost sectors

--- packages/d2-lost-sector-worker/src/index.mjs
+++ packages/d2-lost-sector-worker/src/index.mjs
@@ -2,12 +2,12 @@ import { DefinitionHandler } from '@the-traveler-times/bungie-api-gateway'
 import { getCurrentLostSectorHashes } from './LostSectorHandler'
 
 const modifierIgnoreList = [
-  '2687456355', // Champions: Cabal
-  '1930311099', // Champions: Vex
-  '3495411183', // Champions: Taken
-  '2834348323', // Champions: Mob
-  '2055950944', // Champions: Fallen
-  '3605663348', // Champions: Hive
+  // '2687456355', // Champions: Cabal
+  // '1930311099', // Champions: Vex
+  // '3495411183', // Champions: Taken
+  // '2834348323', // Champions: Mob
+  // '2055950944', // Champions: Fallen
+  // '3605663348', // Champions: Hive
   '2078602635', // Champions: All
   '939324719', // Equipment Locked
   '3859784314', // Match Game
@@ -16,13 +16,60 @@ const modifierIgnoreList = [
   '376634891', // Limited Revives
 ]
 
+const modifierDetailsMap = {
+  'Desolate Charge': {
+    damageType: 'arc',
+    primaryInformation: '+50% Arc damage',
+    secondaryInformation: 'dealt & recieved.',
+  },
+  'Forlorn Miasma': {
+    damageType: 'void',
+    primaryInformation: '+50% Void damage',
+    secondaryInformation: 'dealt & recieved.',
+  },
+  'Isolated Flames': {
+    damageType: 'solar',
+    primaryInformation: '+50% Solar damage',
+    secondaryInformation: 'dealt & recieved.',
+  },
+}
+
+function getModifiersOfInterest(modifiers) {
+  const modifiersOfInterest = modifiers.filter(
+    modifier => !modifierIgnoreList.includes(modifier.hash),
+  )
+  const champions = modifiersOfInterest
+    .filter(modifier => modifier.displayProperties.name.includes('Champions'))
+    .map(modifier => ({
+      name: modifier.displayProperties.name.replace('Champions: ', ''),
+      icon: modifier.displayProperties.icon,
+      description: modifier.displayProperties.description,
+      hash: modifier.hash,
+    }))
+
+  const damage = modifiersOfInterest
+    .filter(modifier => modifier.displayProperties.name.includes('+50%'))
+    .map(modifier => ({
+      ...modifierDetailsMap[modifier.displayProperties.name],
+      name: modifier.displayProperties.name.replace('Champions: ', ''),
+      icon: modifier.displayProperties.icon,
+      description: modifier.displayProperties.description,
+      hash: modifier.hash,
+    }))
+
+  return {
+    champions,
+    damage,
+  }
+}
+
 async function getLostSectorData(lostSector, definitionHandler) {
   const activity = await definitionHandler.getActivity(lostSector.hash)
   const modifiers = await definitionHandler.getActivityModifiers(
-    ...activity.modifiers
-      .map(modifier => modifier.activityModifierHash)
-      .filter(hash => !modifierIgnoreList.includes(hash)),
+    ...activity.modifiers.map(modifier => modifier.activityModifierHash),
   )
+
+  const modifiersOfInterest = getModifiersOfInterest(modifiers)
 
   const destination = await definitionHandler.getDestination(
     activity.destinationHash,
@@ -37,7 +84,14 @@ async function getLostSectorData(lostSector, definitionHandler) {
     )
   }
 
-  return { ...lostSector, ...activity, modifiers, rewards, destination }
+  return {
+    ...lostSector,
+    ...activity,
+    modifiers,
+    modifiersOfInterest,
+    rewards,
+    destination,
+  }
 }
 
 export default {
