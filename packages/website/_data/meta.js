34bb091ccdfcba6da7540490c32e975b2f8f8a2c
Steve Ledsworth <sledsworth@gmail.com>
2021-07-19 23:36:48 -0400

    renamed pgcr data to meta for website

--- packages/website/_data/meta.js
+++ packages/website/_data/meta.js
@@ -1,7 +1,7 @@
 const { fetchDataFromEndpoint } = require('../data-loader')
 
-const PGCR_ENDPOINT = 'https://d2-pgcr-worker.empatheticbot.workers.dev'
-const DEV_PGCR_ENDPOINT = 'http://localhost:3010'
+const PGCR_ENDPOINT = 'https://d2-meta-worker.empatheticbot.workers.dev/meta'
+const DEV_PGCR_ENDPOINT = 'http://localhost:3011/meta'
 
 module.exports = async function () {
   return fetchDataFromEndpoint(PGCR_ENDPOINT, DEV_PGCR_ENDPOINT)
