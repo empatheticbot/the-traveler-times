70fc98d4fa10a12152dc000d7073854d501dac93
Steve Ledsworth <sledsworth@gmail.com>
2021-08-24 23:29:16 -0400

    added new lost sector and updated rotation

--- packages/d2-lost-sector-worker/src/LostSectorHandler.ts
+++ packages/d2-lost-sector-worker/src/LostSectorHandler.ts
@@ -1,6 +1,8 @@
-const startingDate = new Date('May 14, 2021 17:00:00 GMT')
+const startingDate = new Date('August 25, 2021 17:00:00 GMT')
 
 const availableLostSectors = [
+  { name: 'Perdition', master: '1070981425', legend: '1070981430' },
+  { name: 'Bay of Drowned Wishes', master: '660710120', legend: '660710127' },
   { name: 'The Quarry', master: '3253890600', legend: '3253890607' },
   { name: "Scavenger's Den", master: '1905792146', legend: '1905792149' },
   { name: 'Excavation Site XII', master: '548616653', legend: '548616650' },
@@ -13,14 +15,13 @@ const availableLostSectors = [
   { name: 'K1 Revelation', master: '3911969238', legend: '3911969233' },
   { name: 'Concealed Void', master: '912873274', legend: '912873277' },
   { name: 'Bunker E15', master: '1648125538', legend: '1648125541' },
-  { name: 'Perdition', master: '1070981425', legend: '1070981430' },
 ]
 
 const rewards = [
-  { name: 'Head', master: '2679019194', legend: '1387420892' },
-  { name: 'Legs', master: '247000308', legend: '2850782006' },
   { name: 'Arms', master: '256080248', legend: '1572351682' },
   { name: 'Chest', master: '2686128774', legend: '176055472' },
+  { name: 'Head', master: '2679019194', legend: '1387420892' },
+  { name: 'Legs', master: '247000308', legend: '2850782006' },
 ]
 
 export function getCurrentLostSectorHashes() {
