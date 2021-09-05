646f09c7366f08bafa38b82f273d3ce638c4fd0f
Steve Ledsworth <sledsworth@gmail.com>
2021-09-04 22:55:59 -0400

    added trials section to website

--- packages/d2-trials-worker/src/index.ts
+++ packages/d2-trials-worker/src/index.ts
@@ -8,19 +8,8 @@ export default {
     await twitterHandler.init(env.TWITTER_API)
     const seasonHandler = new SeasonHandler()
     await seasonHandler.init(env.BUNGIE_API)
-    // const activityHandler = new ActivityHandler()
-    // await activityHandler.init(env.BUNGIE_API)
 
     try {
-      //       const nightfallMilestone = await publicMilestoneHandler.getPublicMilestoneByHash(
-      //         '1942283261',
-      //       )
-      //
-      //       const activities = await activityHandler.getActivities(
-      //         nightfallMilestone.activities,
-      //       )
-      //
-      //       const modifierGroups = getModifiersOrderedByDifficulty(activities)
       const trialsMilestone = await publicMilestoneHandler.getPublicMilestoneByHash(Hashes.TRIALS)
       const ironBannerMilestone = await publicMilestoneHandler.getPublicMilestoneByHash(Hashes.IRON_BANNER)
       const isIronBannerWeek = !!ironBannerMilestone
@@ -32,7 +21,14 @@ export default {
         const startDate = new Date(trialsMilestone.startDate)
         trialsMaps = await twitterHandler.getTrialsMap(startDate)
       }
-      return new Response(JSON.stringify({ isFirstWeekOfSeason, isIronBannerWeek, isAvailable, trialsMaps, trialsMilestone }), {
+      
+      return new Response(JSON.stringify({ 
+        isFirstWeekOfSeason,
+        isIronBannerWeek, 
+        isAvailable, 
+        trialsMaps, 
+        trialsMilestone 
+      }), {
         status: 200,
       })
     } catch (e) {
