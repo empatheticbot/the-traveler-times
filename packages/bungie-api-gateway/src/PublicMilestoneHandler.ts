ff27fed08e5876bf84df36aa0a725b68e551b53b
Steve Ledsworth <sledsworth@gmail.com>
2021-09-10 16:10:26 -0400

    added trials map to API

--- packages/bungie-api-gateway/src/PublicMilestoneHandler.ts
+++ packages/bungie-api-gateway/src/PublicMilestoneHandler.ts
@@ -1,5 +1,5 @@
 import BungieAPIHandler from './BungieAPIHandler'
-
+import { IRON_BANNER } from './Hashes'
 export default class PublicMilestoneHandler {
   async init(bungieApiEnv) {
     this.bungieAPIHandler = new BungieAPIHandler()
@@ -30,5 +30,22 @@ export default class PublicMilestoneHandler {
       return milestone
     }
     throw new Error('Milestone hash id not found.')
+  }
+  // TODO: use this for weekly iron banner fetching
+  async getIronBannerMilestone() {
+    let milestone
+    try {
+      milestone =
+        await this.getPublicMilestoneByHash(
+          IRON_BANNER
+        )
+    } catch (e) {
+      console.log('Iron Banner not available.')
+      return { isAvailable: false }
+    }
+    return {
+      ...milestone,
+      isAvailable: true,
+    }
   }
 }
--- packages/d2-trials-worker/src/index.ts
+++ packages/d2-trials-worker/src/index.ts
@@ -1,30 +1,32 @@
-import { PublicMilestoneHandler, TwitterHandler, SeasonHandler, Hashes } from '@the-traveler-times/bungie-api-gateway'
+import { PublicMilestoneHandler, TwitterHandler, SeasonHandler, VendorHandler, Hashes } from '@the-traveler-times/bungie-api-gateway'
 
 export default {
   async fetch(request, env) {
     const publicMilestoneHandler = new PublicMilestoneHandler()
     await publicMilestoneHandler.init(env.BUNGIE_API)
+    const vendorHandler = new VendorHandler()
+    await vendorHandler.init(env.BUNGIE_API)
     const twitterHandler = new TwitterHandler()
     await twitterHandler.init(env.TWITTER_API)
     const seasonHandler = new SeasonHandler()
     await seasonHandler.init(env.BUNGIE_API)
 
     try {
+      const { weeklyReset, weekendReset } = await vendorHandler.getWeeklyResets()
       const trialsMilestone = await publicMilestoneHandler.getPublicMilestoneByHash(Hashes.TRIALS)
-      const ironBannerMilestone = await publicMilestoneHandler.getPublicMilestoneByHash(Hashes.IRON_BANNER)
-      const isIronBannerWeek = !!ironBannerMilestone
+      const ironBannerMilestone = await publicMilestoneHandler.getIronBannerMilestone()
+      const isIronBannerWeek = ironBannerMilestone.isAvailable
       const isFirstWeekOfSeason = await seasonHandler.isFirstWeekOfSeason()
 
       const isAvailable = !isIronBannerWeek && !isFirstWeekOfSeason
       let trialsMaps
       if (isAvailable) {
-        const startDate = new Date(trialsMilestone.startDate)
+        const startDate = new Date(weekendReset)
+        startDate.setDate(startDate.getDate() - 7)
         trialsMaps = await twitterHandler.getTrialsMap(startDate)
       }
       
       return new Response(JSON.stringify({ 
-        isFirstWeekOfSeason,
-        isIronBannerWeek, 
         isAvailable, 
         trialsMaps, 
         trialsMilestone 
