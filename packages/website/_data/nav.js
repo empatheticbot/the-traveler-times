9edfe262deaa05a01603f2eb6ae98d212dd3cd17
Steve Ledsworth <sledsworth@gmail.com>
2022-01-03 22:50:58 -0500

    initial implementation of curcible and vendor prose navigation

--- packages/website/_data/nav.js
+++ packages/website/_data/nav.js
@@ -29,20 +29,112 @@ module.exports = async function () {
     weekly,
   ] = await Promise.all(promises)
 
+  let crucibleMarkup
+  let crucibleDate
+  if (trials.isAvailable) {
+    crucibleDate = trials.startDate
+    crucibleMarkup = `
+    <section>
+    <since-date datetime="${crucibleDate}"></since-date>
+    <p>
+      <a href="#trials">Trials</a> is now available. Be sure to check out [<a href="#meta">The Meta</a>
+      <a href="#meta-kills">Kills</a>, <a href="#meta-usage">Usage</a>, and <a href="#meta-efficiency">Efficiency</a>] before entering the fray.
+    </p>
+    </section>
+  `
+  } else if (weekly.ironBanner.isAvailable) {
+    crucibleDate = weekly.ironBanner.startDate
+    crucibleMarkup = `
+    <section>
+    <since-date datetime="${crucibleDate}"></since-date>
+    <p>
+      <a href="#iron-banner">Iron Banner</a> is now available. Be sure to check out [<a href="#meta">The Meta</a>
+      <a href="#meta-kills">Kills</a>, <a href="#meta-usage">Usage</a>, and <a href="#meta-efficiency">Efficiency</a>] before entering the fray.
+    </p>
+    </section>
+  `
+  } else {
+    crucibleMarkup = `
+    <section>
+    <p>
+    Check out [<a href="#meta">The Meta</a>
+      <a href="#meta-kills">Kills</a>, <a href="#meta-usage">Usage</a>, and <a href="#meta-efficiency">Efficiency</a>] to get an edge on your opponents in the Crucible.
+    </p>
+    </section>
+  `
+  }
+
+  let vendorMarkup
+  let vendorDate
+  if (vendors.xur.isAvailable) {
+    vendorDate = vendors.xur.lastRefreshDate
+    vendorMarkup = `
+    <section>
+    <since-date datetime="${vendorDate}"></since-date>
+    <p>
+      ${vendors.xur.name} has been located in the ${vendors.xur.location.area}, ${vendors.xur.location.planet}! Looking for something else? Check out the other <a href="#vendors">Vendors</a> [<a href="#xur">Xur</a>
+      <a href="#spider">Spider</a>, <a href="#banshee-44">Banshee-44</a>, and <a href="#ada-1">Ada-1</a>] stock.
+    </p>
+    </section>
+    `
+  } else {
+    vendorMarkup = `
+    <section>
+    <p>
+    Check out the <a href="#vendors">Vendors</a> [<a href="#xur">Xur</a>
+      <a href="#spider">Spider</a>, <a href="#banshee-44">Banshee-44</a>, and <a href="#ada-1">Ada-1</a>] stock.
+    </p>
+    </section>
+    `
+  }
+  // console.log(
+  //   meta.isAvailable,
+  //   bungieRss.isAvailable,
+  //   lostSectors.isAvailable,
+  //   nightfall.isAvailable,
+  //   season.isAvailable,
+  //   trials.isAvailable,
+  //   vendors.isAvailable,
+  //   Object.values(vendors).map(
+  //     (vendor) => `${vendor.name} -> ${vendor.enabled}`
+  //   ),
+  //   weekly.isAvailable,
+  //   weekly.ironBanner.isAvailable
+  // )
+  const activitiesAndDates = [
+    { activity: bungieRss, date: bungieRss.lastUpdateDate },
+    { activity: lostSectors, date: lostSectors.startDate },
+    { activity: nightfall, date: nightfall.startDate },
+    { activity: trials, date: trials.startDate },
+    { activity: vendors.xur, date: vendors.xur.lastRefreshDate },
+    // { activity: vendors.spider, date: vendors.spider.lastRefreshDate },
+    // { activity: vendors.banshee, date: vendors.banshee.lastRefreshDate },
+    // { activity: vendors.ada, date: vendors.ada.lastRefreshDate },
+    { activity: weekly.ironBanner, date: weekly.ironBanner.startDate },
+  ]
+
+  const orderedActivities = activitiesAndDates
+    .filter((item) => item.activity.isAvailable)
+    .sort((itemA, itemB) => new Date(itemB.date) - new Date(itemA.date))
+
+  // console.log(
+  //   bungieRss.lastUpdateDate,
+  //   lostSectors.startDate,
+  //   nightfall.startDate,
+  //   season.currentSeason.startDate,
+  //   season.currentSeason.endDate,
+  //   trials.startDate,
+  //   Object.values(vendors).map(
+  //     (vendor) =>
+  //       `${vendor.name}: ${vendor.lastRefreshDate} -> ${vendor.nextRefreshDate}`
+  //   ),
+  //   weekly.nextWeeklyReset,
+  //   weekly.nextWeekendReset,
+  //   weekly.ironBanner.startDate
+  // )
   console.log(
-    bungieRss.lastUpdateDate,
-    lostSectors.startDate,
-    nightfall.startDate,
-    season.currentSeason.startDate,
-    season.currentSeason.endDate,
-    trials.startDate,
-    Object.values(vendors).map(
-      (vendor) => `${vendor.lastRefreshDate} -> ${vendor.nextRefreshDate}`
-    ),
-    weekly.nextWeeklyReset,
-    weekly.nextWeekendReset,
-    weekly.ironBanner.startDate
+    orderedActivities.map((item) => `${item.date} - ${item.activity.name}`)
   )
 
-  return [meta, vendors, weekly]
+  return [vendorMarkup, crucibleMarkup]
 }
