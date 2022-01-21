ec20f271fbd8225ef98d1384a450475f6fb2d826
Steven Ledsworth <sledsworth@gmail.com>
2022-01-21 18:30:30 -0500

    added live social images to site

---
+++ packages/website/_data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+  return {
+    title: 'The Traveler Times',
+    url: 'https://thetraveler.news/',
+    socialImageURL: `https://loving-bassi-5578d9.netlify.app/https%3A%2F%2Fthetraveler.news/large/1:1/larger/_${new Date().toISOString()}`,
+    description: 'Keep up to date on the latest Destiny 2 happenings!',
+    author: {
+      name: 'Empatheticbot LLC',
+      email: 'steve@empatheticbot.com',
+    },
+  }
+}
