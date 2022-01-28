e5c85a69efc0ee81aeae751ffc8dbfd32f35ca1b
Steven Ledsworth <sledsworth@gmail.com>
2022-01-28 17:19:44 -0500

    updated website builder to be less aggressive and removed cache purge and netlify buidl

--- packages/website-builder-worker/src/index.ts
+++ packages/website-builder-worker/src/index.ts
@@ -17,50 +17,15 @@ async function buildTravelerTimesWebsite(request, env) {
   )
 }
 
-async function buildTravelerTimesWebsiteNetlify(env) {
-  const netlifyBuildURL = env.NETLIFY_BUILD_URL
-  return fetch(netlifyBuildURL, {
-    method: 'POST',
-  })
-}
-
-async function purgeWebsiteCache(env) {
-  const zone_id = env.ZONE_ID
-
-  const init = {
-    'content-type': 'application/json;charset=UTF-8',
-    method: 'POST',
-    headers: {
-      'X-Auth-Key': env.SECRET_AUTH_KEY,
-      'X-Auth-Email': env.SECRET_AUTH_EMAIL,
-    },
-    body: JSON.stringify({
-      purge_everything: true,
-    }),
-  }
-  return fetch(
-    `https://api.cloudflare.com/client/v4/zones/${zone_id}/purge_cache`,
-    init
-  )
-}
-
 export default {
   async fetch(request, env) {
     if (!isAuthorized(request, env)) {
       return new Response('Unauthorized', { status: 401 })
     }
 
-    setTimeout(() => {
-      purgeWebsiteCache(env)
-    }, 1000 * 60 * 4)
-    buildTravelerTimesWebsiteNetlify(env)
     return buildTravelerTimesWebsite(request, env)
   },
   async scheduled(event, env) {
-    setTimeout(() => {
-      purgeWebsiteCache(env)
-    }, 1000 * 60 * 4)
-    buildTravelerTimesWebsiteNetlify(env)
     return buildTravelerTimesWebsite(null, env)
   },
 }
