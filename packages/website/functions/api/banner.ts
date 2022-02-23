744d7bf4b4fa144113a467528a48f2d4e12735fd
Steven Ledsworth <sledsworth@gmail.com>
2022-02-23 16:16:03 +0000

    added simple error handling in banner api

--- packages/website/functions/api/banner.ts
+++ packages/website/functions/api/banner.ts
@@ -1,6 +1,10 @@
 const BANNER = '$BANNER'
 
 export async function onRequestGet({ env }) {
-  const banner = await env.LiveData.get(BANNER, { type: 'json' })
-  return new Response(JSON.stringify(banner))
+  try {
+    const banner = await env.LiveData.get(BANNER, { type: 'json' })
+    return new Response(JSON.stringify(banner))
+  } catch (e) {
+    return new Response(JSON.stringify({ message: e.message }), { status: 500 })
+  }
 }
