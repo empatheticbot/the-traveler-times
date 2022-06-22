ac9b647afe5d71297cf32f18ef5c5093ace5c66f
Steven Ledsworth <sledsworth@gmail.com>
2022-06-22 18:24:40 -0400

    revert for now until I can figure out how to get cloudflare functions to play nice with the monorepo

---
+++ packages/website/functions/api/search/[user].ts
@@ -0,0 +1,11 @@
+// import { BungieAPIHandler } from '@the-traveler-times/bungie-api-gateway'
+
+export async function onRequestGet({ params, env }) {
+  // const bungieAPIHandler = new BungieAPIHandler()
+  // await bungieAPIHandler.init(env.BungieAPI)
+
+  // const userResponse = await bungieAPIHandler.getUsersFromSearch(params.user)
+
+  // return new Response(JSON.stringify(userResponse))
+  return new Response('hello world')
+}
