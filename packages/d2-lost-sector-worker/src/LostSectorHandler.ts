c5c92691ed316a2776e4f88434fccd0ada9e9b82
Steven Ledsworth <sledsworth@gmail.com>
2022-08-25 14:31:02 -0400

    update lost sector today

--- packages/d2-lost-sector-worker/src/LostSectorHandler.ts
+++ packages/d2-lost-sector-worker/src/LostSectorHandler.ts
@@ -3,10 +3,6 @@ const startingDate = new Date('August 23, 2022 17:00:00 GMT')
 const availableLostSectors = [
 	{ name: 'Concealed Void', master: '912873274', legend: '912873277' },
 	{ name: 'Bunker E15', master: '1648125538', legend: '1648125541' },
-	{ name: 'K1 Crew Quarters', master: '184186578', legend: '184186581' },
-	{ name: 'K1 Logistics', master: '567131519', legend: '567131512' },
-	{ name: 'K1 Revelation', master: '3911969238', legend: '3911969233' },
-	{ name: 'K1 Communion', master: '2829206720', legend: '2829206727' },
 	{ name: 'The Conflux', master: '1163502296', legend: '1163502303' },
 	{ name: 'Metamorphosis', master: '3678847134', legend: '3678847129' },
 	{ name: 'Sepulcher', master: '480864721', legend: '480864726' },
@@ -19,6 +15,10 @@ const availableLostSectors = [
 		overrides: { pgcrImage: '/img/destiny_content/pgcr/skydock_iv.jpg' },
 	},
 	{ name: 'The Quarry', master: '1344654779', legend: '1344654780' },
+	{ name: 'K1 Crew Quarters', master: '184186578', legend: '184186581' },
+	{ name: 'K1 Logistics', master: '567131519', legend: '567131512' },
+	{ name: 'K1 Revelation', master: '3911969238', legend: '3911969233' },
+	{ name: 'K1 Communion', master: '2829206720', legend: '2829206727' },
 	// NOTE: Disabled Lost Sectors
 	// { name: 'Veles Labyrinth', master: '3094493727', legend: '3094493720' },
 	// { name: 'Exodus Garden 2A', master: '2936791995', legend: '2936791996' },
